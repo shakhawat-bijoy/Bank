170dcf4e5341cdcd6e814328f3fa60c282043eba
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-10-04 02:01:25 +0600

    right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Bijoy" };
+  const loggedIn = { firstName: "Shakhawat", lastName: "Bijoy", email: "shakhawatbijoy@hotmail.com"};
 
   return (
     <section className="home">
@@ -14,14 +15,25 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transaction efficiently"
           />
-        </header>
 
         <TotalBalanceBox
           accounts={[]}
           totalBanks={1}
           totalCurrentBalance={3000.74}
-        />
+          />
+
+
+
+        </header>
+
+        RECENT TRANSACTION
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 3000.74}, {currentBalance: 3750}]}
+      />  
     </section>
   );
 };
