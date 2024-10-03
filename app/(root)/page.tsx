bffbd0569ebac3c83ab55103c1bf67b14dded5b7
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-10-04 00:00:43 +0600

    left sidebar complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Bijoy" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+        </header>
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={3000.74}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
