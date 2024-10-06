a4e95ffe18b264ef605ddb61793ad359cda273d9
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-10-06 21:27:19 +0600

    ?

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -3,7 +3,7 @@ import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({
   accounts = [], totalBanks, totalCurrentBalance
-}: TotalBalanceBoxProps) => {
+}: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
